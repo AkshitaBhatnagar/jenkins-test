a17fec47dd4e37bdea2146cdceead469ca151572
AkshitaBhatnagar <85331790+AkshitaBhatnagar@users.noreply.github.com>
2025-01-09 21:58:17 +0530

    Update turbonomic.groovy

--- jenkins/turbonomic.groovy
+++ jenkins/turbonomic.groovy
@@ -22,40 +22,38 @@ pipeline {
         )
     }
     stages {
-    stage('Load Util') {
-        options {
-            timeout(time: 60, unit: 'MINUTES')  // Timeout set to 60 minutes
-            retry(1)                            // Retry once if the stage fails
-        }
-        steps {
-            script {
-                echo "Loading Util"
-                echo "Current workspace: ${pwd()}"
-                echo "Listing files in the workspace:"
-                
-                // List all files recursively in the workspace to verify file location
-                sh 'ls -R'
-
-                
-	    // Attempt to load the util.groovy script
-	       turbo_util = load 'utils/util.groovy'
-	       echo "Loaded turbo_util: ${turbo_util}"
-	       echo "Loaded turbo_util: ${turbo_util}"
-	       
-               echo "Methods in turbo_util: ${turbo_util.metaClass.methods.collect { it.name }}"
-
-
-	    // Check if the method exists in the loaded object
-	       if (turbo_util) {
-		   echo "UpdateInstance method exists: ${turbo_util.UpdateInstance != null}"
-	    }  else {
-		 echo "Failed to load turbo_util correctly"
-	    }
-	 
+    	stage('Load Util') {
+            options {
+                timeout(time: 60, unit: 'MINUTES')
+                retry(1)
+            }
+            steps {
+                script {
+                    try {
+                        echo "Loading Util"
+                        echo "Current workspace: ${pwd()}"
+                        echo "Listing files in the workspace:"
+                        sh 'ls -R'
+                        // Attempt to load the util.groovy script
+                        turbo_util = load 'utils/util.groovy'
+                        echo "Loaded turbo_util: ${turbo_util}"
+                        echo "Methods in turbo_util: ${turbo_util.metaClass.methods.collect { it.name }}"
+                        
+                        // Check if the method exists in the loaded object
+                        if (turbo_util && turbo_util.metaClass.hasMetaMethod('UpdateInstance')) {
+                            echo "UpdateInstance method exists"
+                        } else {
+                            echo "UpdateInstance method does not exist!"
+                            error "UpdateInstance method not found"
+                        }
+                    } catch (Exception e) {
+                        echo "Error during Load Util: ${e.message}"
+                        currentBuild.result = 'FAILURE'
+                        error "Failed to load turbo_util: ${e.message}"
+                    }
+                }
             }
         }
-    }
-
 
         stage('Inspect Environment Variables') {
             steps {
