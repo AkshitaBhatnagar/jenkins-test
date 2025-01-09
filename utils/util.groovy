45ff1376dae9fe74b0cb6d46d1dd17753844c905
AkshitaBhatnagar <85331790+AkshitaBhatnagar@users.noreply.github.com>
2025-01-09 17:18:19 +0530

    Update util.groovy

--- utils/util.groovy
+++ utils/util.groovy
@@ -3,23 +3,23 @@ import groovy.transform.Field
 @Field
 def APPLICATION_CONFIG = [
 
-        "244127287648": [ "applicationName": "cedw", "filepath": "aws/cedw/etl.tfvars", "type": "terraform" ],
-        "770017448000": [ "applicationName": "nass", "filepath": "aws/nass/create-stack-management.template", "type":"cfn" ],
+        "Containers": [ "applicationName": "cedw", "filepath": "aws/cedw/etl.tfvars", "type": "terraform" ],
+        "Development": [ "applicationName": "nass", "filepath": "aws/nass/create-stack-management.template", "type":"cfn" ],
     
 ]
 
 
 def UpdateInstance(GITHUB_USERNAME, GITHUB_TOKEN, payload_account, currententity, newentity) {
 
-    def GITHUB_REPO = 'https://github.com/nbnco/TBM_OPS.git'
-    // def GITHUB_REPO = 'https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/nbnco/TBM_OPS.git' 
-    def BRANCH = 'master'  // Ensure this is the correct branch name
+    def GITHUB_REPO = 'https://github.com/AkshitaBhatnagar/jenkins-test.git'
+    // def GITHUB_REPO = 'https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/jenkins-test.git' 
+    def BRANCH = 'main'  // Ensure this is the correct branch name
     def COMMIT_MESSAGE = 'Updated Instance type from turbonomic via Jenkins pipeline'
     
     // Clone the repository
-    sh "git clone https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/nbnco/TBM_OPS.git"
+    sh "git clone https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/jenkins-test.git"
     
-    dir('TBM_OPS') {
+    dir('jenkins-test') {
         def changesMade = false  // Flag to track if changes are made
         def appConfig = APPLICATION_CONFIG[payload_account]
         if (appConfig) {
@@ -77,10 +77,8 @@ def UpdateInstance(GITHUB_USERNAME, GITHUB_TOKEN, payload_account, currententity
             echo "Changes detected. Committing and pushing to GitHub."
             sh """
                 git add .
-                git config user.name "Akshita"
-                git config user.email "akshitabhatnagar@nbnco.com.au"
                 git commit -m "${COMMIT_MESSAGE}"
-                git push https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/nbnco/TBM_OPS.git ${BRANCH}
+                git push https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/jenkins-test.git ${BRANCH}
             """
         } else {
             // No changes, skip commit and push
